867be6c616c89ad1483500572cd5e86240d2e9a2
Lili Cosic <lili@kinvolk.io>
2018-01-25 16:07:56 +0100

    Add main and server pkgs

--- cmd/servicebroker/main.go
+++ cmd/servicebroker/main.go
@@ -1,5 +1,85 @@
-package servicebroker
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"path"
+	"strconv"
+	"syscall"
+
+	"github.com/golang/glog"
+
+	"github.com/pmorie/go-open-service-broker-skeleton/pkg/broker"
+	"github.com/pmorie/go-open-service-broker-skeleton/pkg/server"
+)
+
+var options struct {
+	Port    int
+	TLSCert string
+	TLSKey  string
+}
+
+func init() {
+	flag.IntVar(&options.Port, "port", 8005, "use '--port' option to specify the port for broker to listen on")
+	flag.StringVar(&options.TLSCert, "tlsCert", "", "base-64 encoded PEM block to use as the certificate for TLS. If '--tlsCert' is used, then '--tlsKey' must also be used. If '--tlsCert' is not used, then TLS will not be used.")
+	flag.StringVar(&options.TLSKey, "tlsKey", "", "base-64 encoded PEM block to use as the private key matching the TLS certificate. If '--tlsKey' is used, then '--tlsCert' must also be used")
+	flag.Parse()
+}
 
 func main() {
+	if err := run(); err != nil && err != context.Canceled && err != context.DeadlineExceeded {
+		glog.Fatalln(err)
+	}
+}
 
+func run() error {
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	defer cancelFunc()
+	go cancelOnInterrupt(ctx, cancelFunc)
+
+	return runWithContext(ctx)
+}
+
+func runWithContext(ctx context.Context) error {
+	if flag.Arg(0) == "version" {
+		fmt.Printf("%s/%s\n", path.Base(os.Args[0]), "0.1.0")
+		return nil
+	}
+	if (options.TLSCert != "" || options.TLSKey != "") &&
+		(options.TLSCert == "" || options.TLSKey == "") {
+		fmt.Println("To use TLS, both --tlsCert and --tlsKey must be used")
+		return nil
+	}
+
+	addr := ":" + strconv.Itoa(options.Port)
+
+	api := broker.NewAPISurface()
+	s := server.New(*api)
+
+	var err error
+	if options.TLSCert == "" && options.TLSKey == "" {
+		err = s.Run(ctx, addr)
+	} else {
+		err = s.RunTLS(ctx, addr, options.TLSCert, options.TLSKey)
+	}
+	return err
+}
+
+func cancelOnInterrupt(ctx context.Context, f context.CancelFunc) {
+	term := make(chan os.Signal)
+	signal.Notify(term, os.Interrupt, syscall.SIGTERM)
+
+	for {
+		select {
+		case <-term:
+			glog.Infof("Received SIGTERM, exiting gracefully...")
+			f()
+			os.Exit(0)
+		case <-ctx.Done():
+			os.Exit(0)
+		}
+	}
 }
