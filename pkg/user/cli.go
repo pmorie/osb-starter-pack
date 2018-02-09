ef27f08d869b6e9908b64a158b6cdfbbb2ade1fa
Lili Cosic <lili@kinvolk.io>
2018-02-09 14:03:19 +0100

    Add async cli flag to enable asynchronous requests

--- pkg/user/cli.go
+++ pkg/user/cli.go
@@ -5,10 +5,11 @@ import (
 )
 
 // Options holds the options specified by the user's code on the command
-// line.  Users should add their own options here and add flags for them in
+// line. Users should add their own options here and add flags for them in
 // AddUserFlags.
 type Options struct {
 	CatalogPath string
+	Async       bool
 }
 
 // AddUserFlags is a hook called to initialize the CLI flags for user options it
@@ -16,4 +17,5 @@ type Options struct {
 // called.
 func AddUserFlags(o *Options) {
 	flag.StringVar(&o.CatalogPath, "catalogPath", "", "The path to the catalog")
+	flag.BoolVar(&o.Async, "async", false, "Indicates whether the broker is handling the requests asynchronously.")
 }
