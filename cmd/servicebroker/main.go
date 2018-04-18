16a8b551275c3c58f4a45232839bdf59f6d17ebb
Jay Boyd <jaboyd@redhat.com>
2018-04-17 21:20:29 -0400

    by default serve securely (TLS)

--- cmd/servicebroker/main.go
+++ cmd/servicebroker/main.go
@@ -22,15 +22,21 @@ import (
 var options struct {
 	broker.Options
 
-	Port    int
-	TLSCert string
-	TLSKey  string
+	Port        int
+	Insecure    bool
+	TLSCert     string
+	TLSKey      string
+	TLSCertFile string
+	TLSKeyFile  string
 }
 
 func init() {
-	flag.IntVar(&options.Port, "port", 8005, "use '--port' option to specify the port for broker to listen on")
-	flag.StringVar(&options.TLSCert, "tlsCert", "", "base-64 encoded PEM block to use as the certificate for TLS. If '--tlsCert' is used, then '--tlsKey' must also be used. If '--tlsCert' is not used, then TLS will not be used.")
-	flag.StringVar(&options.TLSKey, "tlsKey", "", "base-64 encoded PEM block to use as the private key matching the TLS certificate. If '--tlsKey' is used, then '--tlsCert' must also be used")
+	flag.IntVar(&options.Port, "port", 8443, "use '--port' option to specify the port for broker to listen on")
+	flag.BoolVar(&options.Insecure, "insecure", false, "use --insecure to use HTTP vs HTTPS.")
+	flag.StringVar(&options.TLSCertFile, "tls-cert-file", "", "File containing the default x509 Certificate for HTTPS. (CA cert, if any, concatenated after server cert).")
+	flag.StringVar(&options.TLSKeyFile, "tls-private-key-file", "", "File containing the default x509 private key matching --tls-cert-file.")
+	flag.StringVar(&options.TLSCert, "tlsCert", "", "base-64 encoded PEM block to use as the certificate for TLS. If '--tlsCert' is used, then '--tlsKey' must also be used.")
+	flag.StringVar(&options.TLSKey, "tlsKey", "", "base-64 encoded PEM block to use as the private key matching the TLS certificate.")
 	broker.AddFlags(&options.Options)
 	flag.Parse()
 }
@@ -56,7 +62,7 @@ func runWithContext(ctx context.Context) error {
 	}
 	if (options.TLSCert != "" || options.TLSKey != "") &&
 		(options.TLSCert == "" || options.TLSKey == "") {
-		fmt.Println("To use TLS, both --tlsCert and --tlsKey must be used")
+		fmt.Println("To use TLS with specified cert or key data, both --tlsCert and --tlsKey must be used")
 		return nil
 	}
 
@@ -81,10 +87,20 @@ func runWithContext(ctx context.Context) error {
 
 	glog.Infof("Starting broker!")
 
-	if options.TLSCert == "" && options.TLSKey == "" {
+	if options.Insecure {
 		err = s.Run(ctx, addr)
 	} else {
-		err = s.RunTLS(ctx, addr, options.TLSCert, options.TLSKey)
+		if options.TLSCert != "" && options.TLSKey != "" {
+			glog.V(4).Infof("Starting secure broker with TLS cert and key data")
+			err = s.RunTLS(ctx, addr, options.TLSCert, options.TLSKey)
+		} else {
+			if options.TLSCertFile == "" || options.TLSKeyFile == "" {
+				glog.Error("unable to run securely without TLS Certificate and Key. Please review options and if running with TLS, specify --tls-cert-file and --tls-private-key-file or --tlsCert and --tlsKey.")
+				return nil
+			}
+			glog.V(4).Infof("Starting secure broker with file based TLS cert and key")
+			err = s.RunTLSWithTLSFiles(ctx, addr, options.TLSCertFile, options.TLSKeyFile)
+		}
 	}
 	return err
 }
